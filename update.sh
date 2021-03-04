5a56dabcd32e7b446f939a75d9f3deb55b26417c
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-04 17:57:17 +0700

    Fix: updater script

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -e "Updating Libernet ..." \
-  && git pull \
+  && git fetch origin master \
+  && git reset --hard FETCH_HEAD \
   && bash ./install.sh \
   && echo -e "\nLibernet successfully updated!"
