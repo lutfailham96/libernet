28db1324faabfab3ec1f1daf7c632d34fd39b678
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-14 20:12:04 +0700

    Fixed js file

--- web/index.php
+++ web/index.php
@@ -91,11 +91,7 @@
         <?php include('footer.php'); ?>
     </div>
 </div>
-<script src="lib/vendor/jquery/jquery-3.6.0.slim.min.js"></script>
-<script src="lib/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="lib/vendor/vuejs/vue.min.js"></script>
-<script src="lib/vendor/axios/axios.min.js"></script>
-<script src="lib/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+<?php include('js.php'); ?>
 <script>
     let vm = new Vue({
         el: '#app',
--- web/login.php
+++ web/login.php
@@ -65,8 +65,4 @@
         <?php include('footer.php'); ?>
     </div>
 </div>
-<script src="lib/vendor/jquery/jquery-3.6.0.slim.min.js"></script>
-<script src="lib/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="lib/vendor/vuejs/vue.min.js"></script>
-<script src="lib/vendor/axios/axios.min.js"></script>
-<script src="lib/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+<?php include('js.php'); ?>
