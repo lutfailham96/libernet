f58e63ca5e168ada60a7e0e1fd33119549f8a257
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-14 20:11:22 +0700

    Add: js.php file

---
+++ web/js.php
@@ -0,0 +1,6 @@
+<script src="lib/vendor/jquery/jquery-3.6.0.slim.min.js"></script>
+<script src="lib/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="lib/vendor/vuejs/vue.min.js"></script>
+<script src="lib/vendor/axios/axios.min.js"></script>
+<script src="lib/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+<script src="lib/vendor/lodash/lodash.min.js"></script>
