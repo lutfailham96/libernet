a0bae8b8422459a50aeabd5c3c978dc83300df31
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-18 16:29:02 +0700

    Moving to version 1.4.10

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.4.9. All rights reserved.
+    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.4.10. All rights reserved.
 </footer>
