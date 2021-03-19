af41586a74f14820a24bf8d85d273917b8d35776
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-19 22:58:20 +0700

    New style engine

--- bin/ssh-ssl-loop.sh
+++ bin/ssh-ssl-loop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# SSH-SSL Loop
+# SSH-SSL Loop Wrapper
 # by Lutfa Ilham
-# v1.1
+# v1.0
 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
 
-function connect_ssh_ssl() {
+function connect() {
   sshpass -p "${2}" ssh \
     -4CND "${3}" \
     -p 10443 \
@@ -20,6 +20,6 @@ function connect_ssh_ssl() {
 
 while true; do
   # command username password dynamic_port
-  connect_ssh_ssl $1 $2 $3
+  connect "${1}" "${2}" "${3}"
   sleep 3
 done
