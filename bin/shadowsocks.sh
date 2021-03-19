8ca926eb7085153d9d05e291524477db845a5151
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-19 22:57:08 +0700

    New style engine

--- bin/shadowsocks.sh
+++ bin/shadowsocks.sh
@@ -2,42 +2,54 @@
 
 # Shadowsocks Wrapper
 # by Lutfa Ilham
-# v1.1
+# v1.0
 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
 
+SERVICE_NAME="Shadowsocks"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
 SHADOWSOCKS_PROFILE="$(grep 'shadowsocks":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
 SHADOWSOCKS_CONFIG="${LIBERNET_DIR}/bin/config/shadowsocks/${SHADOWSOCKS_PROFILE}.json"
 
-function start_shadowsocks() {
-  screen -AmdS ss-client ss-local -c "${SHADOWSOCKS_CONFIG}"
+function run() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${SHADOWSOCKS_PROFILE}, Mode: ${SERVICE_NAME}"
+  "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
+  echo -e "Starting ${SERVICE_NAME} service ..."
+  screen -AmdS ss-client ss-local -c "${SHADOWSOCKS_CONFIG}" \
+    && echo -e "${SERVICE_NAME} service started!"
 }
 
-function stop_shadowsocks() {
+function stop() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
+  echo -e "Stopping ${SERVICE_NAME} service ..."
   kill $(screen -list | grep ss-client | awk -F '[.]' {'print $1'})
   # kill plugins
   killall obfs-local
   killall ck-client
+  echo -e "${SERVICE_NAME} service stopped!"
 }
 
-while getopts ":rs" opt; do
-  case ${opt} in
-  r)
-    start_shadowsocks > /dev/null 2>&1
-    echo -e "Shadowsocks started!"
+function usage() {
+  cat <<EOF
+Usage:
+  -r  Run ${SERVICE_NAME} service
+  -s  Stop ${SERVICE_NAME} service
+EOF
+}
+
+case "${1}" in
+  -r)
+    run
     ;;
-  s)
-    stop_shadowsocks > /dev/null 2>&1
-    echo -e "Shadowsocks stopped!"
+  -s)
+    stop
     ;;
   *)
-    echo -e "Usage:"
-    echo -e "\t-r\tRun Shadowsocks"
-    echo -e "\t-s\tStop Shadowsocks"
+    usage
     ;;
-  esac
-done
+esac
