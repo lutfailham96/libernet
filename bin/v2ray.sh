ef02299757e782dc2fdbdb7a25749c6b144561a0
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-19 22:57:24 +0700

    New style engine

--- bin/v2ray.sh
+++ bin/v2ray.sh
@@ -2,43 +2,51 @@
 
 # V2Ray Wrapper
 # by Lutfa Ilham
-# v1.1
+# v1.0
 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
 
+SERVICE_NAME="V2Ray"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-declare -x V2RAY_PROFILE
+V2RAY_PROFILE="$(grep 'v2ray":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
+V2RAY_CONFIG="${LIBERNET_DIR}/bin/config/v2ray/${V2RAY_PROFILE}.json"
 
-function start_v2ray() {
-  if [[ -z ${1} ]];then
-    V2RAY_PROFILE="$(grep 'v2ray":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-    screen -AmdS v2ray-client v2ray -c "${LIBERNET_DIR}/bin/config/v2ray/${V2RAY_PROFILE}.json"
-  else
-    screen -AmdS v2ray-client v2ray -c "${LIBERNET_DIR}/bin/config/v2ray/${1}.json"
-  fi
+function run() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${V2RAY_PROFILE}, Mode: ${SERVICE_NAME}"
+  "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
+  echo -e "Starting ${SERVICE_NAME} service ..."
+  screen -AmdS v2ray-client v2ray -c "${V2RAY_CONFIG}" \
+    && echo -e "${SERVICE_NAME} service started!"
 }
 
-function stop_v2ray() {
+function stop() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
+  echo -e "Stopping ${SERVICE_NAME} service ..."
   kill $(screen -list | grep v2ray-client | awk -F '[.]' {'print $1'})
+  echo -e "${SERVICE_NAME} service stopped!"
 }
 
-while getopts ":rs" opt; do
-  case ${opt} in
-  r)
-    start_v2ray $2 > /dev/null 2>&1
-    echo "V2Ray started!"
+function usage() {
+  cat <<EOF
+Usage:
+  -r  Run ${SERVICE_NAME} service
+  -s  Stop ${SERVICE_NAME} service
+EOF
+}
+
+case "${1}" in
+  -r)
+    run
     ;;
-  s)
-    stop_v2ray > /dev/null 2>&1
-    echo "V2Ray stopped!"
+  -s)
+    stop
     ;;
   *)
-    echo -e "Usage:"
-    echo -e "\t-r\tRun V2Ray"
-    echo -e "\t-s\tStop V2Ray"
+    usage
     ;;
-  esac
-done
+esac
