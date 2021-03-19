c8826760a2d1a9c0fc0de339c4ff65959ff4ef1e
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-19 23:00:46 +0700

    New style engine

--- bin/ssh.sh
+++ bin/ssh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# SSH Connector
+# SSH Connector Wrapper
 # by Lutfa Ilham
-# v1.1
+# v1.0
 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
 
+SERVICE_NAME="SSH"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
 SSH_PROFILE="$(grep 'ssh":' ${SYSTEM_CONFIG}  | awk '{print $2}' | sed 's/,//g; s/"//g')"
 SSH_CONFIG="${LIBERNET_DIR}/bin/config/ssh/${SSH_PROFILE}.json"
@@ -21,32 +22,50 @@ PROXY_PORT="$(grep 'port":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"/
 DYNAMIC_PORT="$(grep 'port":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
 ENABLE_HTTP="$(grep 'enable_http":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
 
-function start_ssh() {
-  if [[ $ENABLE_HTTP == 'true' ]]; then
-    screen -AmdS ssh-connector "${LIBERNET_DIR}/bin/ssh-loop.sh" -e "${SSH_USER}" "${SSH_PASS}" "${SSH_HOST}" "${SSH_PORT}" "${DYNAMIC_PORT}" "${PROXY_IP}" "${PROXY_PORT}"
+function run() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${SSH_PROFILE}, Mode: ${SERVICE_NAME}"
+  if [[ "${ENABLE_HTTP}" == 'true' ]]; then
+    "${LIBERNET_DIR}/bin/http.sh" -r \
+      && "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service" \
+      && echo -e "Starting ${SERVICE_NAME} service ..." \
+      && screen -AmdS ssh-connector "${LIBERNET_DIR}/bin/ssh-loop.sh" -e "${SSH_USER}" "${SSH_PASS}" "${SSH_HOST}" "${SSH_PORT}" "${DYNAMIC_PORT}" "${PROXY_IP}" "${PROXY_PORT}" \
+      && echo -e "${SERVICE_NAME} service started!"
   else
+    # write to service log
+    "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
+    echo -e "Starting ${SERVICE_NAME} service ..."
     screen -AmdS ssh-connector "${LIBERNET_DIR}/bin/ssh-loop.sh" -d "${SSH_USER}" "${SSH_PASS}" "${SSH_HOST}" "${SSH_PORT}" "${DYNAMIC_PORT}"
   fi
 }
 
-function stop_ssh() {
+function stop() {
+  # write to service log
+  "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
+  echo -e "Stopping ${SERVICE_NAME} service ..."
   kill $(screen -list | grep ssh-connector | awk -F '[.]' {'print $1'})
+  echo -e "${SERVICE_NAME} service stopped!"
+  if [[ "${ENABLE_HTTP}" == 'true' ]]; then
+    "${LIBERNET_DIR}/bin/http.sh" -s
+  fi
 }
 
-while getopts ":rs" opt; do
-  case ${opt} in
-  r)
-    start_ssh > /dev/null 2>&1
-    echo -e "SSH started!"
+function usage() {
+  cat <<EOF
+Usage:
+  -r  Run ${SERVICE_NAME} service
+  -s  Stop ${SERVICE_NAME} service
+EOF
+}
+
+case "${1}" in
+  -r)
+    run
     ;;
-  s)
-    stop_ssh > /dev/null 2>&1
-    echo -e "SSH stopped!"
+  -s)
+    stop
     ;;
   *)
-    echo -e "Usage:"
-    echo -e "\t-r\tRun SSH"
-    echo -e "\t-s\tStop SSH"
+    usage
     ;;
-  esac
-done
+esac
