8b9a8c08c2cb76a1b49ff38a9164d68cf9fc2ccb
Lutfa Ibtihaji Ilham <lutfailham@nocta.my.id>
2021-03-22 19:24:34 +0700

    Use http-proxy service looper to prevent program exit

--- bin/http.sh
+++ bin/http.sh
@@ -19,7 +19,7 @@ function run() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
   echo -e "Starting ${SERVICE_NAME} service ..."
-  screen -AmdS http-proxy python3 -u "${LIBERNET_DIR}/bin/http.py" "${SSH_CONFIG}" -l ${LISTEN_PORT} \
+  screen -AmdS http-proxy bash -c "while true; do python3 -u \"${LIBERNET_DIR}/bin/http.py\" \"${SSH_CONFIG}\" -l ${LISTEN_PORT}; sleep 3; done" \
     && echo -e "${SERVICE_NAME} service started!"
 }
 
@@ -28,6 +28,7 @@ function stop() {
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
   kill $(screen -list | grep http-proxy | awk -F '[.]' {'print $1'})
+  killall python3
   echo -e "${SERVICE_NAME} service stopped!"
 }
 
