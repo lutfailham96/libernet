fb6742a723604609c1dd1a16a76ceb8d42cc7ffd
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-03-25 21:28:19 +0700

    Iptables: maximum wait to acquire xtables lock before give up

--- bin/dns.sh
+++ bin/dns.sh
@@ -16,8 +16,8 @@ function run() {
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
   echo -e "Starting ${SERVICE_NAME} service ..."
   # initialize iptables
-  iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
-  iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
+  iptables -w -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
+  iptables -w -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
   screen -AmdS stubby stubby -C "${LIBERNET_DIR}/bin/config/dns/stubby.yml" -v 0 \
     && echo -e "${SERVICE_NAME} service started!"
 }
@@ -27,8 +27,8 @@ function stop() {
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
   # remove iptables
-  iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
-  iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
+  iptables -w -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
+  iptables -w -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5453
   kill $(screen -list | grep stubby | awk -F '[.]' {'print $1'})
   killall stubby
   echo -e "${SERVICE_NAME} service stopped!"
