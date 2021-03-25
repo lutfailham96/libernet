b6afac3d202bceeb4b0d8520c15a7672dfe2c34c
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-03-25 17:07:32 +0700

    Moving to version v1.5.2

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.1. All rights reserved.
+    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.2. All rights reserved.
 </footer>
