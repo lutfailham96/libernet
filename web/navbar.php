8c2cd559c71bcacf5ff0031a270a049fe0d516ea
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-03-27 16:08:21 +0700

    Add: speedtest web ui

--- web/navbar.php
+++ web/navbar.php
@@ -18,6 +18,9 @@
             <li class="nav-item <?php if ($url === 'system.php') echo 'active'; ?>">
                 <a class="nav-link" href="system.php">System</a>
             </li>
+            <li class="nav-item <?php if ($url === 'speedtest.php') echo 'active'; ?>">
+                <a class="nav-link" href="speedtest.php">SpeedTest</a>
+            </li>
             <li class="nav-item <?php if ($url === 'about.php') echo 'active'; ?>">
                 <a class="nav-link" href="about.php">About</a>
             </li>
---
+++ web/speedtest.php
@@ -0,0 +1,36 @@
+<?php
+    include('auth.php');
+    check_session();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+        $title = "SpeedTest";
+        include("head.php");
+    ?>
+</head>
+<body>
+<div id="app">
+    <?php include('navbar.php'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-12 mx-auto mt-4 mb-2">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">SpeedTest</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="//openspeedtest.com/Get-widget.php" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('footer.php'); ?>
+    </div>
+</div>
+<?php include("javascript.php"); ?>
+</body>
+</html>
