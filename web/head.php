561c2d41368dee95133d1308532759d797327d37
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-04-08 22:45:48 +0700

    Add: fontawesome css

--- web/head.php
+++ web/head.php
@@ -2,8 +2,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<!-- Font Awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="lib/vendor/bootstrap/css/bootstrap.min.css">
+<!-- Main CSS -->
 <link rel="stylesheet" href="css/style.css">
 
 <title>Libernet | <?= $title ?></title>
