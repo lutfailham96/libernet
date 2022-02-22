40708760c15920a938e5cf1b18ea9fd3949fce06
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-02-22 13:02:47 +0700

    Prevent delete empty config

--- web/js/config.js
+++ web/js/config.js
@@ -357,6 +357,7 @@ const app = new Vue({
             })
         },
         deleteConfig() {
+            if (this.config.profile === '--- Empty ---') return
             Swal.fire({
                 title: 'Are you sure?',
                 text: "You won't be able to revert this!",
