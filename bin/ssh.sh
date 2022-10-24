cabd30931ee712a348060b7f811bddcdf80c5628
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-24 23:58:20 +0700

    Refactor `ssh.sh`

--- bin/ssh.sh
+++ bin/ssh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # SSH Connector Wrapper
 # by Lutfa Ilham
@@ -11,18 +11,18 @@ fi
 
 SERVICE_NAME="SSH"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-SSH_PROFILE="$(grep 'ssh":' ${SYSTEM_CONFIG}  | awk '{print $2}' | sed 's/,//g; s/"//g')"
+SSH_PROFILE=$(grep 'ssh":' "${SYSTEM_CONFIG}"  | awk '{print $2}' | sed 's/,//g; s/"//g')
 SSH_CONFIG="${LIBERNET_DIR}/bin/config/ssh/${SSH_PROFILE}.json"
-SSH_HOST="$(grep 'host":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-SSH_PORT="$(grep 'port":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
-SSH_USER="$(grep 'username":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-SSH_PASS="$(grep 'password":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-PROXY_IP="$(grep 'ip":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '3p')"
-PROXY_PORT="$(grep 'port":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '3p')"
-DYNAMIC_PORT="$(grep 'port":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
-ENABLE_HTTP="$(grep 'enable_http":' ${SSH_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
+SSH_HOST=$(grep 'host":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+SSH_PORT=$(grep 'port":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
+SSH_USER=$(grep 'username":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+SSH_PASS=$(grep 'password":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+PROXY_IP=$(grep 'ip":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '3p')
+PROXY_PORT=$(grep 'port":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '3p')
+DYNAMIC_PORT=$(grep 'port":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
+ENABLE_HTTP=$(grep 'enable_http":' "${SSH_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
 
-function run() {
+run() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${SSH_PROFILE}, Mode: ${SERVICE_NAME}"
   if [[ "${ENABLE_HTTP}" == 'true' ]]; then
@@ -39,18 +39,18 @@ function run() {
   fi
 }
 
-function stop() {
+stop() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
-  kill $(screen -list | grep ssh-connector | awk -F '[.]' {'print $1'})
+  kill "$(screen -list | grep ssh-connector | awk -F '[.]' '{print $1}')"
   echo -e "${SERVICE_NAME} service stopped!"
   if [[ "${ENABLE_HTTP}" == 'true' ]]; then
     "${LIBERNET_DIR}/bin/http.sh" -s
   fi
 }
 
-function usage() {
+usage() {
   cat <<EOF
 Usage:
   -r  Run ${SERVICE_NAME} service
