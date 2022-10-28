72743208b3cf07a2ea54cad4b201c60fe4632db8
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-28 08:30:31 +0700

    Refactor `tun2socks.sh`

--- bin/tun2socks.sh
+++ bin/tun2socks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Tun2socks Wrapper
 # by Lutfa Ilham
@@ -10,112 +10,119 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-TUN2SOCKS_MODE="$(grep 'legacy":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_DEV="$(grep 'dev":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_ADDRESS="$(grep 'address":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_NETMASK="$(grep 'netmask":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_GATEWAY="$(grep 'gateway":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_MTU="$(grep 'mtu":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-SOCKS_IP="$(grep 'ip":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
-SOCKS_PORT="$(grep 'port":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
+TUN2SOCKS_MODE=$(grep 'legacy":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_DEV=$(grep 'dev":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_ADDRESS=$(grep 'address":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_NETMASK=$(grep 'netmask":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_GATEWAY=$(grep 'gateway":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_MTU=$(grep 'mtu":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+SOCKS_IP=$(grep 'ip":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
+SOCKS_PORT=$(grep 'port":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
 SOCKS_SERVER="${SOCKS_IP}:${SOCKS_PORT}"
-UDPGW_IP="$(grep 'ip":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '2p')"
-UDPGW_PORT="$(grep 'port":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '2p')"
+UDPGW_IP=$(grep 'ip":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '2p')
+UDPGW_PORT=$(grep 'port":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '2p')
 UDPGW="${UDPGW_IP}:${UDPGW_PORT}"
-GATEWAY="$(ip route | grep -v tun | awk '/default/ { print $3 }')"
-SERVER_IP="$(grep 'server":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
-CDN_IP="$(grep 'cdn_server":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')"
-readarray -t PROXY_IPS < <(jq -r '.proxy_servers[]' < ${SYSTEM_CONFIG})
-readarray -t DNS_IPS < <(jq -r '.dns_servers[]' < ${SYSTEM_CONFIG})
+GATEWAY=$(ip route | grep -v tun | awk '/default/ { print $3 }')
+SERVER_IP=$(grep 'server":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
+CDN_IP=$(grep 'cdn_server":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g' | sed -n '1p')
+readarray -t PROXY_IPS < <(jq -r '.proxy_servers[]' < "${SYSTEM_CONFIG}")
+readarray -t DNS_IPS < <(jq -r '.dns_servers[]' < "${SYSTEM_CONFIG}")
 ROUTE_LOG="${LIBERNET_DIR}/log/route.log"
-DEFAULT_ROUTE="$(ip route show | grep default)"
+DEFAULT_ROUTE=$(ip route show | grep default)
+DEFAULT_METRIC="10"
 
-function init_tun_dev {
+init_tun_dev() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Tun2socks: initializing tun device"
   # remove tun dev if already exist
   if ifconfig "${TUN_DEV}" > /dev/null 2>&1; then
-    ifconfig ${TUN_DEV} down
-    ip tuntap del dev ${TUN_DEV} mode tun
+    ifconfig "${TUN_DEV}" down
+    ip tuntap del dev "${TUN_DEV}" mode tun
   fi
   # finally init tun dev
-  ip tuntap add dev ${TUN_DEV} mode tun
-  ifconfig ${TUN_DEV} mtu ${TUN_MTU}
+  ip tuntap add dev "${TUN_DEV}" mode tun
+  ifconfig "${TUN_DEV}" mtu "${TUN_MTU}"
   echo -e "Tun device initialized!"
 }
 
-function destroy_tun_dev {
+destroy_tun_dev() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Tun2socks: removing tun device"
-  ifconfig ${TUN_DEV} down
-  ip tuntap del dev ${TUN_DEV} mode tun
+  ifconfig "${TUN_DEV}" down
+  ip tuntap del dev "${TUN_DEV}" mode tun
   echo -e "Tun device removed!"
 }
 
-function start_tun2socks {
+start_tun2socks() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting tun2socks service"
-  ifconfig ${TUN_DEV} ${TUN_GATEWAY} netmask ${TUN_NETMASK} up
+  ifconfig "${TUN_DEV}" "${TUN_GATEWAY}" netmask "${TUN_NETMASK}" up
   if [[ $TUN2SOCKS_MODE == "false" ]]; then
     screen -AmdS go-tun2socks go-tun2socks -loglevel none -proxyServer "${SOCKS_SERVER}" -proxyType socks -tunName "${TUN_DEV}" -tunAddr "${TUN_ADDRESS}" -tunGw "${TUN_GATEWAY}" -tunMask "${TUN_NETMASK}"
+    # using another go-tun2socks
+    #screen -AmdS go-tun2socks go-tun2socks -loglevel silent -device "${TUN_DEV}" -proxy socks5://"${SOCKS_SERVER}" -tcp-auto-tuning
   else
-    screen -AmdS badvpn-tun2socks badvpn-tun2socks --loglevel 0 --tundev ${TUN_DEV} --netif-ipaddr ${TUN_ADDRESS} --netif-netmask ${TUN_NETMASK} --socks-server-addr ${SOCKS_SERVER} --udpgw-remote-server-addr "${UDPGW}"
+    screen -AmdS badvpn-tun2socks badvpn-tun2socks --loglevel 0 --tundev "${TUN_DEV}" --netif-ipaddr "${TUN_ADDRESS}" --netif-netmask "${TUN_NETMASK}" --socks-server-addr "${SOCKS_SERVER}" --udpgw-remote-server-addr "${UDPGW}"
+    # using another tun2socks
+    #screen -AmdS tun2socks tun2socks -loglevel silent -device "${TUN_DEV}" -proxy socks5://"${SOCKS_SERVER}"
   fi
   # removing default route
-  echo ${DEFAULT_ROUTE} > ${ROUTE_LOG} \
-    && ip route del ${DEFAULT_ROUTE}
+  echo "${DEFAULT_ROUTE}" > "${ROUTE_LOG}" \
+    && ip route del "${DEFAULT_ROUTE}"
   # add default route to tun2socks
-  ip route add default via ${TUN_ADDRESS} metric 6
+  ip route add default via "${TUN_ADDRESS}" metric ${DEFAULT_METRIC}
   echo -e "Tun2socks started!"
   # write connected time
   "${LIBERNET_DIR}/bin/log.sh" -c "$(date +"%s")"
 }
 
-function stop_tun2socks {
+stop_tun2socks() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping tun2socks service"
   if [[ $TUN2SOCKS_MODE == "false" ]]; then
-    kill $(screen -list | grep go-tun2socks | awk -F '[.]' {'print $1'})
+    kill "$(screen -list | grep go-tun2socks | awk -F '[.]' '{print $1}')"
   else
-    kill $(screen -list | grep badvpn-tun2socks | awk -F '[.]' {'print $1'})
+    kill "$(screen -list | grep badvpn-tun2socks | awk -F '[.]' '{print $1}')"
+    # using another tun2socks
+    #kill "$(screen -list | grep tun2socks | awk -F '[.]' '{print $1}')"
   fi
   # recover default route
-  ip route add $(cat "${ROUTE_LOG}") \
+  ip route add "$(cat "${ROUTE_LOG}")" \
     && rm -rf "${ROUTE_LOG}"
   # remove default route to tun2socks
-  ip route del default via ${TUN_ADDRESS} metric 6
+  ip route del default via "${TUN_ADDRESS}" metric ${DEFAULT_METRIC}
   echo -e "Tun2socks stopped!"
 }
 
-function route_add_ip {
+route_add_ip() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Tun2socks: routing server, proxy and DNS IPs"
-  ip route add ${SERVER_IP} via ${GATEWAY} metric 4 &
-  ip route add ${CDN_IP} via ${GATEWAY} metric 4 &
+  ip route add "${SERVER_IP}" via "${GATEWAY}" metric ${DEFAULT_METRIC} &
+  ip route add "${CDN_IP}" via "${GATEWAY}" metric ${DEFAULT_METRIC} &
   for IP in "${PROXY_IPS[@]}"; do
-    ip route add ${IP} via ${GATEWAY} metric 4 &
+    ip route add "${IP}" via "${GATEWAY}" metric ${DEFAULT_METRIC} &
   done
   for IP in "${DNS_IPS[@]}"; do
-    ip route add ${IP} via ${GATEWAY} metric 4 &
+    ip route add "${IP}" via "${GATEWAY}" metric ${DEFAULT_METRIC} &
   done
   echo -e "Routes initialized!"
 }
 
-function route_del_ip {
+route_del_ip() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Tun2socks: removing routes"
   for IP in "${DNS_IPS[@]}"; do
-    ip route del ${IP} &
+    ip route del "${IP}" &
   done
   for IP in "${PROXY_IPS[@]}"; do
-    ip route del ${IP} &
+    ip route del "${IP}" &
   done
-  ip route del ${CDN_IP} &
-  ip route del ${SERVER_IP} &
+  ip route del "${CDN_IP}" &
+  ip route del "${SERVER_IP}" &
   echo -e "Routes removed!"
 }
 
-function usage() {
+usage() {
   cat <<EOF
 Usage:
   -i  Initialize tun device
@@ -128,6 +135,14 @@ EOF
 }
 
 case "${1}" in
+  -ar)
+    echo -e "Setup routes ..."
+    route_add_ip
+    ;;
+  -dr)
+    echo -e "Removing routes ..."
+    route_del_ip
+    ;;
   -v)
     # start tun2socks service
     init_tun_dev
