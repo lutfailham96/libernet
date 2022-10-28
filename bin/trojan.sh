6d082026528283f56ca8519321d4334e00a45c4b
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-28 08:12:24 +0700

    Refactor `trojan.sh`

--- bin/trojan.sh
+++ bin/trojan.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Trojan Wrapper
 # by Lutfa Ilham
@@ -11,10 +11,10 @@ fi
 
 SERVICE_NAME="Trojan"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-TROJAN_PROFILE="$(grep 'trojan":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
+TROJAN_PROFILE=$(grep 'trojan":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
 TROJAN_CONFIG="${LIBERNET_DIR}/bin/config/trojan/${TROJAN_PROFILE}.json"
 
-function run() {
+run() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${TROJAN_PROFILE}, Mode: ${SERVICE_NAME}"
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
@@ -23,16 +23,16 @@ function run() {
     && echo -e "${SERVICE_NAME} service started!"
 }
 
-function stop() {
+stop() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
-  kill $(screen -list | grep trojan-client | awk -F '[.]' {'print $1'})
+  kill "$(screen -list | grep trojan-client | awk -F '[.]' '{print $1}')"
   killall trojan-go
   echo -e "${SERVICE_NAME} service stopped!"
 }
 
-function usage() {
+usage() {
   cat <<EOF
 Usage:
   -r  Run ${SERVICE_NAME} service
