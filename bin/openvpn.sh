912f80fbee8d8e6926f06f9db974a8e1b3cb21e4
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-28 08:09:04 +0700

    Refactor `openvpn.sh`

--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # OpenVPN Wrapper
 # by Lutfa Ilham
@@ -11,26 +11,26 @@ fi
 
 SERVICE_NAME="OpenVPN"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-OPENVPN_PROFILE="$(grep 'openvpn":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
+OPENVPN_PROFILE=$(grep 'openvpn":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
 OPENVPN_CONFIG="${LIBERNET_DIR}/bin/config/openvpn/${OPENVPN_PROFILE}.json"
-OPENVPN_OVPN="$(grep 'ovpn":' ${OPENVPN_CONFIG} | awk -F ': "' '{ print $2 }' | sed 's/",//g')"
+OPENVPN_OVPN=$(grep 'ovpn":' "${OPENVPN_CONFIG}" | awk -F ': "' '{ print $2 }' | sed 's/",//g')
 OPENVPN_CFG="${LIBERNET_DIR}/bin/config/openvpn/${OPENVPN_PROFILE}.ovpn"
 OPENVPN_CRED="${LIBERNET_DIR}/bin/config/openvpn/${OPENVPN_PROFILE}.txt"
-OPENVPN_HOST="$(echo -e ${OPENVPN_OVPN} | grep 'remote ' | awk '{print $2}')"
-OPENVPN_PORT="$(echo -e ${OPENVPN_OVPN} | grep 'remote ' | awk '{print $3}')"
-OPENVPN_USER="$(grep 'username":' ${OPENVPN_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-OPENVPN_PASS="$(grep 'password":' ${OPENVPN_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-OPENVPN_SNI="$(grep 'sni":' ${OPENVPN_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-ENABLE_SSL="$(grep 'ssl":' ${OPENVPN_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-TUN_DEV="$(grep 'dev":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
-GATEWAY="$(ip route | grep -v tun | awk '/default/ { print $3 }')"
-SERVER_IP="$(ping  -4Ac 1 -W 1 ${OPENVPN_HOST} | awk '{print $3}' | sed -n '1p' | sed 's/(//g; s/)//g; s/://g')"
-readarray -t PROXY_IPS < <(jq -r '.proxy_servers[]' < ${SYSTEM_CONFIG})
-readarray -t DNS_IPS < <(jq -r '.dns_servers[]' < ${SYSTEM_CONFIG})
+OPENVPN_HOST=$(echo -e "${OPENVPN_OVPN}" | grep 'remote ' | awk '{print $2}')
+OPENVPN_PORT=$(echo -e "${OPENVPN_OVPN}" | grep 'remote ' | awk '{print $3}')
+OPENVPN_USER=$(grep 'username":' "${OPENVPN_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+OPENVPN_PASS=$(grep 'password":' "${OPENVPN_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+OPENVPN_SNI=$(grep 'sni":' "${OPENVPN_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+ENABLE_SSL=$(grep 'ssl":' "${OPENVPN_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+TUN_DEV=$(grep 'dev":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
+GATEWAY=$(ip route | grep -v tun | awk '/default/ { print $3 }')
+SERVER_IP=$(ping  -4Ac 1 -W 1 "${OPENVPN_HOST}" | awk '{print $3}' | sed -n '1p' | sed 's/(//g; s/)//g; s/://g')
+readarray -t PROXY_IPS < <(jq -r '.proxy_servers[]' < "${SYSTEM_CONFIG}")
+readarray -t DNS_IPS < <(jq -r '.dns_servers[]' < "${SYSTEM_CONFIG}")
 ROUTE_LOG="${LIBERNET_DIR}/log/route.log"
-DEFAULT_ROUTE="$(ip route show | grep default | grep -v ${TUN_DEV})"
+#DEFAULT_ROUTE=$(ip route show | grep default | grep -v "${TUN_DEV}")
 
-function configure() {
+configure() {
   # generate ovpn
   echo -e "${OPENVPN_OVPN}" > "${OPENVPN_CFG}"
   # change tun dev
@@ -39,35 +39,35 @@ function configure() {
   sed -i "s/remote .*/remote ${OPENVPN_HOST} ${OPENVPN_PORT}/g" "${OPENVPN_CFG}"
   # auth
   echo -e "${OPENVPN_USER}\n${OPENVPN_PASS}" > "${OPENVPN_CRED}"
-  sed -i "s/auth-user-pass.*/auth-user-pass \"$(echo ${OPENVPN_CRED} | sed 's/\//\\\//g')\"/g" "${OPENVPN_CFG}"
+  sed -i "s/auth-user-pass.*/auth-user-pass \"$(echo "${OPENVPN_CRED}" | sed 's/\//\\\//g')\"/g" "${OPENVPN_CFG}"
   # remove up & down resolve
   sed -i "/up .*/d" "${OPENVPN_CFG}"
   sed -i "/down .*/d" "${OPENVPN_CFG}"
 }
 
-function route() {
+route() {
   case "${1}" in
     -a)
       # route server
       ip route add "${SERVER_IP}" via "${GATEWAY}" metric 4
       # route proxy & dns
       for IP in "${PROXY_IPS[@]}"; do
-        ip route add ${IP} via ${GATEWAY} metric 4 &
+        ip route add "${IP}" via "${GATEWAY}" metric 4 &
       done
       for IP in "${DNS_IPS[@]}"; do
-        ip route add ${IP} via ${GATEWAY} metric 4 &
+        ip route add "${IP}" via "${GATEWAY}" metric 4 &
       done
       ;;
     -d)
       # recover default route, from service
-      ip route add $(cat "${ROUTE_LOG}") \
+      ip route add "$(cat "${ROUTE_LOG}")" \
         && rm -rf "${ROUTE_LOG}"
       # remove proxy & dns route
       for IP in "${DNS_IPS[@]}"; do
-       ip route del ${IP}
+       ip route del "${IP}"
       done
       for IP in "${PROXY_IPS[@]}"; do
-        ip route del ${IP}
+        ip route del "${IP}"
       done
       # remove server route
       ip route del "${SERVER_IP}"
@@ -75,7 +75,7 @@ function route() {
   esac
 }
 
-function run() {
+run() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${OPENVPN_PROFILE}, Mode: ${SERVICE_NAME}"
   route -a
@@ -91,11 +91,11 @@ function run() {
     && echo -e "${SERVICE_NAME} service started!"
 }
 
-function stop() {
+stop() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
-  kill $(screen -list | grep openvpn | awk -F '[.]' {'print $1'})
+  kill "$(screen -list | grep openvpn | awk -F '[.]' '{print $1}')"
   killall openvpn
   if [[ "${ENABLE_SSL}" == 'true' ]]; then
     "${LIBERNET_DIR}/bin/stunnel.sh" -s
@@ -106,7 +106,7 @@ function stop() {
   echo -e "${SERVICE_NAME} service stopped!"
 }
 
-function usage() {
+usage() {
   cat <<EOF
 Usage:
   -r  Run ${SERVICE_NAME} service
