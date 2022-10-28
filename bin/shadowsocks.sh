27ffec4def7aef00ab7e001a1f7fb8e4da89fcd0
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-28 08:10:41 +0700

    Refactor `shadowsocks.sh`

--- bin/shadowsocks.sh
+++ bin/shadowsocks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Shadowsocks Wrapper
 # by Lutfa Ilham
@@ -11,10 +11,10 @@ fi
 
 SERVICE_NAME="Shadowsocks"
 SYSTEM_CONFIG="${LIBERNET_DIR}/system/config.json"
-SHADOWSOCKS_PROFILE="$(grep 'shadowsocks":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; s/"//g')"
+SHADOWSOCKS_PROFILE=$(grep 'shadowsocks":' "${SYSTEM_CONFIG}" | awk '{print $2}' | sed 's/,//g; s/"//g')
 SHADOWSOCKS_CONFIG="${LIBERNET_DIR}/bin/config/shadowsocks/${SHADOWSOCKS_PROFILE}.json"
 
-function run() {
+run() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Config: ${SHADOWSOCKS_PROFILE}, Mode: ${SERVICE_NAME}"
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
@@ -23,11 +23,11 @@ function run() {
     && echo -e "${SERVICE_NAME} service started!"
 }
 
-function stop() {
+stop() {
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
-  kill $(screen -list | grep ss-client | awk -F '[.]' {'print $1'})
+  kill "$(screen -list | grep ss-client | awk -F '[.]' '{print $1}')"
   killall ss-local
   # kill plugins
   killall obfs-local
@@ -35,7 +35,7 @@ function stop() {
   echo -e "${SERVICE_NAME} service stopped!"
 }
 
-function usage() {
+usage() {
   cat <<EOF
 Usage:
   -r  Run ${SERVICE_NAME} service
