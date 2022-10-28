e231ceef8bcc02e280ef36eb69ee29ed10e3b42a
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-28 10:28:31 +0700

    Update `dns.sh`

--- bin/dns.sh
+++ bin/dns.sh
@@ -10,15 +10,24 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 SERVICE_NAME="DNS Resolver"
+SCREEN_NAME="stubby"
+
+check() {
+  if screen -list | grep -q "${SCREEN_NAME}"; then
+    echo -e "${SERVICE_NAME} service already running, exiting ..."
+    exit 1
+  fi
+}
 
 run() {
+  check
   # write to service log
   "${LIBERNET_DIR}/bin/log.sh" -w "Starting ${SERVICE_NAME} service"
   echo -e "Starting ${SERVICE_NAME} service ..."
   # initialize iptables
-  iptables -w -t nat -A OUTPUT ! -d 127.0.0.1 -p udp --dport 53 -j REDIRECT --to-ports 5453
-  iptables -w -t nat -A OUTPUT ! -d 127.0.0.1 -p tcp --dport 53 -j REDIRECT --to-ports 5453
-  screen -AmdS stubby stubby -C "${LIBERNET_DIR}/config/dns/stubby.yml" -v 0 \
+  iptables -w -t nat -A OUTPUT ! -d 127.0.0.1 -p udp --dport 53 -j REDIRECT --to-ports 5453 2> /dev/null
+  iptables -w -t nat -A OUTPUT ! -d 127.0.0.1 -p tcp --dport 53 -j REDIRECT --to-ports 5453 2> /dev/null
+  screen -AmdS "${SCREEN_NAME}" stubby -C "${LIBERNET_DIR}/config/dns/stubby.yml" -v 0 \
     && echo -e "${SERVICE_NAME} service started!"
 }
 
@@ -27,10 +36,10 @@ stop() {
   "${LIBERNET_DIR}/bin/log.sh" -w "Stopping ${SERVICE_NAME} service"
   echo -e "Stopping ${SERVICE_NAME} service ..."
   # remove iptables
-  iptables -w -t nat -D OUTPUT ! -d 127.0.0.1 -p udp --dport 53 -j REDIRECT --to-ports 5453
-  iptables -w -t nat -D OUTPUT ! -d 127.0.0.1 -p tcp --dport 53 -j REDIRECT --to-ports 5453
-  kill "$(screen -list | grep stubby | awk -F '[.]' '{print $1}')"
-  killall stubby
+  iptables -w -t nat -D OUTPUT ! -d 127.0.0.1 -p udp --dport 53 -j REDIRECT --to-ports 5453 2> /dev/null
+  iptables -w -t nat -D OUTPUT ! -d 127.0.0.1 -p tcp --dport 53 -j REDIRECT --to-ports 5453 2> /dev/null
+  kill "$(screen -list | grep "${SCREEN_NAME}" | awk -F '[.]' '{print $1}')" 2> /dev/null
+  killall stubby 2> /dev/null
   echo -e "${SERVICE_NAME} service stopped!"
 }
 
